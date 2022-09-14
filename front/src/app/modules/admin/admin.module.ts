5f76f07f7596cd364902577d3f49dd11a136e19a
cbalucas <cbalucas@hotmail.com>
2022-09-14 20:04:20 -0300

    Admin

--- front/src/app/modules/admin/admin.module.ts
+++ front/src/app/modules/admin/admin.module.ts
@@ -1,26 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CabezaComponent } from './admin-cabeza/admin_cabeza.component';
-import { CuerpoComponent } from './admin-cuerpo/admin-cuerpo.component';
-import { PiesComponent } from './admin-pies/admin-pies.component';
-
 
 
 
 @NgModule({
-  declarations: [
-    CabezaComponent,
-    CuerpoComponent,
-    PiesComponent,
-  ],
+  declarations: [],
   imports: [
     CommonModule
-
-  ],
-  exports: [
-    CabezaComponent,
-    CuerpoComponent,
-    PiesComponent
   ]
 })
-export class HomeModule { }
+export class AdminModule { }
