85780fcd8498eac041aab12696ed6d73673e5f80
cbalucas <cbalucas@hotmail.com>
2022-09-15 23:35:27 -0300

    Ocultar Formularios

--- front/src/app/modules/home/administrador/administrador.component.ts
+++ front/src/app/modules/home/administrador/administrador.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./administrador.component.scss']
 })
 export class AdministradorComponent implements OnInit {
-
-  constructor() { }
+  elementSec = false; //SECCIONES
+  elementCon = false; //CONTACTO
+  elementRSoc = false; //REDES SOCIALES
+ constructor() { }
 
   ngOnInit(): void {
+  }
+  showDataSec() {
+    return (this.elementSec = true);
+  }
+  hideDataSec() {
+    return (this.elementSec = false);
+  }
+  showDataCon() {
+    return (this.elementCon = true);
+  }
+  hideDataCon() {
+    return (this.elementCon = false);
+  }
+  showDataRSoc() {
+    return (this.elementRSoc = true);
+  }
+  hideDataRSoc() {
+    return (this.elementRSoc = false);
   }
 
 }
