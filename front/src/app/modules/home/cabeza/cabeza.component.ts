a1d177753aa23b33216b349e5e1e253f58f5ca71
Carluchox <creges23@gmail.com>
2022-09-22 19:57:19 -0300

    Cambios de mantener fijo el nav-bar del encabezado

--- front/src/app/modules/home/cabeza/cabeza.component.ts
+++ front/src/app/modules/home/cabeza/cabeza.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-cabeza',
   templateUrl: './cabeza.component.html',
   styleUrls: ['./cabeza.component.scss']
 })
 export class CabezaComponent implements OnInit {
+  elementG = false;
+  elementAc = false;
+  elementAt = false;
+  elementAg = false;
+  elementAj =false;
 
   constructor(private router: Router) {}
 
@@ -18,7 +24,24 @@ export class CabezaComponent implements OnInit {
     this.router.navigateByUrl("administrador")
   }
 
-  direction():void{
-    this.router.navigateByUrl("cuerpo")
+  showDataAg() {
+    this.router.navigate("**");
+    return (this.elementAg = true);
+  }
+  
+  showDataAt() {
+    return (this.elementAt = true);
+  }
+  
+  showDataAj() {
+    return (this.elementAj = true);
+  }
+  
+  showDataGa() {
+    return (this.elementG = true);
+  }
+  
+  showDataAc() {
+    return (this.elementAc = true);
   }
 }
