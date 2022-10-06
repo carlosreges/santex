5799a71b199c88b5b2d9c201da675efa25e0d25c
Carluchox <creges23@gmail.com>
2022-10-06 05:49:46 -0300

    sin modificar

--- front/src/app/modules/home/cabeza/cabeza.component.ts
+++ front/src/app/modules/home/cabeza/cabeza.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
-import { CuerpoComponent } from '../cuerpo/cuerpo.component';
-import { UntypedFormBuilder, UntypedFormControl, Validators } from '@angular/forms';
-import { Subscription } from 'rxjs';import { AuthService } from 'src/app/core/services/auth/auth.service';
-import { MAX_PASSWORD_LENGTH, MAX_USERNAME_LENGTH, MIN_PASSWORD_LENGTH, MIN_USERNAME_LENGTH } from 'src/app/core/interfaces/users/users.interface';
-import { ToastService } from 'src/app/core/services/toast/toast.service';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cabeza',
@@ -20,7 +14,6 @@ export class CabezaComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  
   login():void{
     debugger;
     this.router.navigateByUrl("administrador")
