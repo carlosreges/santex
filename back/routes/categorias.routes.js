b434915ffec0cdf54a729420fb1d1da1885444de
cbalucas <cbalucas@hotmail.com>
2022-10-12 00:52:17 -0300

    pruebas

--- back/routes/categorias.routes.js
+++ back/routes/categorias.routes.js
@@ -1,12 +1,15 @@
-/* const Express = require('express');
-const Passport = require('passport');
+const Express = require('express');
+const { ConnectionAcquireTimeoutError, Sequelize } = require('sequelize');
+const { sequelize } = require('../models');
+/*const Passport = require('passport');
 const categoriasController = require('../controllers/categorias.controllers');
 require('../middleware/passport.middleware')(Passport);
 const UserValidator = require('../middleware/validations/users/user.validations.middleware');
-const Validator = require('../middleware/validation.middleware');
-const { sequelize } = require('../models'); */
+const Validator = require('../middleware/validation.middleware');*/
 
+const mysql = require('mysql')
 const app = Express();
+
 
 /* app.post('/login', [
   UserValidator.validate('login'),
@@ -20,22 +23,16 @@ categoriasController.login); */
 ],
 categoriasController.userInfo); */
 
-app.get("",(req,res)=>{
-  res.send("Funciona Usuarios")
-
-/*   pp.get('/categorias',(req,res)=>{
-
-    const sql = 'SELECT * FROM santexacademy.categorias';
-
-    Connection.query(sql,(error,results)=>{
-if (error) throw error;
-if (results.length >0 ){
-    res.json (results);
-
-}else {
-    res.send ('no results');
-}
-
-    }); */
+app.get("", (req,res)=>{
+ res.send("Categorias") 
+  /* const sql = 'SELECT * FROM santexacademy.categorias'; */
+  /* Connection.query(sql,(error,results)=>{ 
+  if (error) throw error;
+      if (results.length >0 ){
+        res.json (results);
+      }else {
+        res.send ('no results');
+      }
+    });*/
 });
 module.exports = app;
