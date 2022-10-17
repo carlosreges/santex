71317a1a61c5dcc6e22339c081374ec9c7023534
cbalucas <cbalucas@hotmail.com>
2022-10-16 22:30:49 -0300

    back

--- back/routes/categorias.routes.js
+++ back/routes/categorias.routes.js
@@ -1,6 +1,8 @@
 /* import {query} from "promise-mysql"; */
 const { config } = require('dotenv');
-const Express = require('express'); 
+const Express = require('express');
+const { sequelize } = require('../models'); 
+const categoriasModels = require('../models/categorias.models');
 
 /* import { response, Router } from "express"; */
 
@@ -13,15 +15,30 @@ const app = Express.Router();
 
  app.get("",async (req,res)=>{ 
     /* const categorias = await sequelize.query("SELECT * FROM `categorias`", { type: QueryTypes.SELECT }); */
-    res.send("Ruta OK");
+    /* res.send("Ruta OK"); */
 /*     const connection = await config();
    const result=await connection.Query("SELECT * FROM santexacademy.categorias");
   
    console.log(result);
    res.json(result);  */
-   });
 
-   
+
+
+const Categorias = await categoriasModels.findAll({atributes:['Categorias']})
+if(Categorias === null){
+    console.log('NO TIENE');
+} else {
+    console.log(Categorias instanceof categoriasModels);
+}
+/* .then(categoria=>{
+    const resultados = JSON.stringify(categoria)
+    console.log(resultados);
+})
+.catch(error=>{
+    console.log(error);
+}) */
+
+});   
 
 
 
