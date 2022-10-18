445017692e2c26bc468a2290f7928eaaaf699206
cbalucas <cbalucas@hotmail.com>
2022-10-18 00:32:00 -0300

    cambios

--- back/models/categorias.models.js
+++ back/models/categorias.models.js
@@ -1,5 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
-  const categoria = sequelize.define('categorias', {
+   
+  const categoria = sequelize.define("categorias", {
     id_Categorias: {
       type: DataTypes.INTEGER(16),
       allowNull: false,
@@ -11,10 +12,12 @@ module.exports = (sequelize, DataTypes) => {
       unique: false,
     },
   }, {
-    timestamps: true,
+    timestamps: false,
     tableName: 'categorias',
   });
-
+/*  (async () => {
+    await sequelize.sync({ force: true }); //CREA LAS TABLAS NUEVAMENTE
+  })();  */
   return categoria;
 };
 
--- back/routes/categorias.routes.js
+++ back/routes/categorias.routes.js
@@ -4,56 +4,9 @@ const Express = require('express');
 const { sequelize } = require('../models'); 
 const categoriasModels = require('../models/categorias.models');
 
-/* import { response, Router } from "express"; */
-
-/* import { methods as languageController } from "../controllers/language.controller"; */
-
 const app = Express.Router();
-/* const router=Router(); */
-/* router.get("/",languageController.getLanguages); */
-
-
- app.get("",async (req,res)=>{ 
-    /* const categorias = await sequelize.query("SELECT * FROM `categorias`", { type: QueryTypes.SELECT }); */
-    /* res.send("Ruta OK"); */
-/*     const connection = await config();
-   const result=await connection.Query("SELECT * FROM santexacademy.categorias");
-  
-   console.log(result);
-   res.json(result);  */
-
-
-
-const Categorias = await categoriasModels.findAll({atributes:['Categorias']})
-if(Categorias === null){
-    console.log('NO TIENE');
-} else {
-    console.log(Categorias instanceof categoriasModels);
-}
-/* .then(categoria=>{
-    const resultados = JSON.stringify(categoria)
-    console.log(resultados);
-})
-.catch(error=>{
-    console.log(error);
-}) */
-
+ app.get("/all",async (req,res)=>{ 
+     res.send("Ruta OK"); 
 });   
 
-
-
-
-
 module.exports = app;
-/* export default router; */
-
-
-/*const connection = mysql.createConnection(config);
-
-connection.connect();
-connection.query('SELECT * FROM categorias', (err, rows, fields)=>{
-    if(err) throw err
-    console.log('the solution is: ', rows[0].solution)
-})
-
-connection.end(); */
