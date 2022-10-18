01e39ec4dc686546e05ce11d6b224b67cc1b09b7
Cristian Aguirre <cristianmoto066@gmail.com>
2022-10-18 02:04:54 -0300

    cambios

--- back/routes/categorias.routes.js
+++ back/routes/categorias.routes.js
@@ -1,41 +1,26 @@
-/* import {query} from "promise-mysql"; */
-/* const { config } = require('dotenv'); */
-const Express = require('express');
-const { ConnectionAcquireTimeoutError, Sequelize } = require('sequelize');
-const { sequelize } = require('../models');
-/*const Passport = require('passport');
-const categoriasController = require('../controllers/categorias.controllers');
-require('../middleware/passport.middleware')(Passport);
-const UserValidator = require('../middleware/validations/users/user.validations.middleware');
-const Validator = require('../middleware/validation.middleware');*/
+const getConnection = require('../config/config');
 
-const mysql = require('mysql')
-const app = Express();
 
-app.get("/all",categoriasController.getCategorias);
+const getCategorias = async(req,res)=>{
+   const connection = await getConnection();
+   const result=await connection.query("SELECT * FROM categorias");
+   console.log(result);
+   res.json(result);
 
-/* app.post('/login', [
-  UserValidator.validate('login'),
-  Validator.checkValidationResult,
-],
-categoriasController.login); */
+}
+const postCategorias =  async (req,res) => {
+    const connection = await getConnection();
+    const result=await connection.query("INSERT INTO categorias(`id_Categorias`,`Categoria`) VALUES(7,'primera')");
+    console.log(result);
+    res.json(result);
 
-// users/info/1
-/*  app.get('/myInfo', [
-  Passport.authenticate('jwt', { session: false }),
-],
-categoriasController.userInfo); */
+}
 
-app.get("", (req,res)=>{
- res.send("Categorias") 
-  /* const sql = 'SELECT * FROM santexacademy.categorias'; */
-  /* Connection.query(sql,(error,results)=>{ 
-  if (error) throw error;
-      if (results.length >0 ){
-        res.json (results);
-      }else {
-        res.send ('no results');
-      }
-    });*/
-});
-module.exports = app;
+/* export const methods = {
+    getCategorias,
+    postCategorias
+}; */
+module.exports = {
+  getCategorias,
+  postCategorias
+};
