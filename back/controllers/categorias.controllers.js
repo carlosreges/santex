8df23c89d1add56140bc1c29345950e4fea5691e
cbalucas <cbalucas@hotmail.com>
2022-10-18 01:08:29 -0300

    back

--- back/controllers/categorias.controllers.js
+++ back/controllers/categorias.controllers.js
@@ -1,11 +1,20 @@
 const mysql = require( 'mysql' );
+const config = require('../config/config');
 
+const getConnection = require('../config/config');
 
+async function getCategorias(req,res){
+   const connection = await getConnection();
+   const result=await connection.query("SELECT * FROM categorias");
+   console.log(result);
+   res.json(result);
 
+};
 
 module.exports = {
-    Database
+  getCategorias,
 };
+
 
 /* class Database {
     constructor( config ) {
--- back/routes/categorias.routes.js
+++ back/routes/categorias.routes.js
@@ -1,12 +1,27 @@
 /* import {query} from "promise-mysql"; */
-const { config } = require('dotenv');
+/* const { config } = require('dotenv'); */
 const Express = require('express');
-const { sequelize } = require('../models'); 
-const categoriasModels = require('../models/categorias.models');
+/* const { sequelize } = require('../models');  */
+const categoriasController = require ( '../controllers/categorias.controllers');
+/* const categoriasModels = require('../models/categorias.models'); */
+
+/* const app=Express();
+app.get("/all",categoriasController.getCategorias()); */
+/* app.get("/all",async (req,res)=>{ 
+     res.send("Ruta OK"); 
+});    */
+
+/* module.exports = app; */
+
+/* import { response, Router } from "express"; */
+/* import { methods as cateoriasControllers } from "../controllers/categorias.controllers"; */
+
 
 const app = Express.Router();
- app.get("/all",async (req,res)=>{ 
-     res.send("Ruta OK"); 
-});   
+
+app.get("/all",categoriasController.getCategorias);
+
+/* app.post("/all", function(req, res){categoriasController.postCategorias}); */
+
 
 module.exports = app;
